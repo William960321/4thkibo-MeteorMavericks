9c553265df0857d2bd7b089675e17d0ddb1b6708
jeremy1214 <s1110280@my.cmsh.cyc.edu.tw>
2023-06-08 19:34:05 +0800

    new java

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -1,5 +1,6 @@
 package jp.jaxa.iss.kibo.rpc.sampleapk;
 
+import android.graphics.drawable.GradientDrawable;
 import android.util.Log;
 
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
@@ -17,62 +18,60 @@ import org.opencv.core.Mat;
  */
 
 public class YourService extends KiboRpcService {
-    // initial value setting
-    final String teamname = "MeteorMavericks";
-    Point point1 = new Point(11.27f, -9.92f,5.3f);
-    Point point2 = new Point(10.61f, -9.07f,4.48f);
-    Point point3 = new Point(10.71f, -7.7f,4.48f);
-    Point point4 = new Point(10.51f, -6.72f,5.18f);
-    Point point5 = new Point(11.11f, -7.98f,5.34f);
-    Point point6 = new Point(11.36f, -8.99f,4.78f);
-    Point point7 = new Point(11.37f, -8.55f,4.48f);
-    //    String mode =
-
     @Override
     protected void runPlan1(){
         // the mission starts
         api.startMission();
-        Log.i(teamname, "start mission");
-        // move to a point
+        int counter = 0;
+        Point[] point = new Point[7];
+        point[0] = new Point(11.2746, -9.92284, 5.2988);
+        point[1] = new Point(10.612 ,-9.0709, 4.48);
+        point[2] = new Point(10.71, -7.7, 4.48);
+        point[3] = new Point(10.51, -6.7185 ,5.1804);
+        point[4] = new Point(11.114 ,-7.9756, 5.3393);
+        point[5] = new Point(11.355 ,-8.9929 ,4.7818);
+        point[6] = new Point(11.369 ,-8.5518, 4.48);
 
-        Quaternion quaternion = new Quaternion(0f, 0f, 0f, 1f);
-        api.moveTo(point1, quaternion, false);
-        Log.i(teamname, "move to 10.71 - 7.5 - 4.48");
-
-        // report point1 arrival
-//        api.reportPoiny1Arrival();
-
-        // get a camera image
-//        Mat image = api.getMatNavCam();
-
-        // irradiate the laser
-        api.laserControl(true);
-
-        // take target1 snapshots
-//        api.takeTarget1Snapshot();
-
-        // turn the laser off
-        api.laserControl(false);
-      int loop_counter = 0;
+        Quaternion[] quaternion = new Quaternion[7];
+        quaternion[0] = new Quaternion(0f, 0f, -0.707f ,0.707f);
+        quaternion[1] = new Quaternion(0.5f, 0.5f, -0.5f, 0.5f);
+        quaternion[2] = new Quaternion(0f, 0.707f, 0f, 0.707f);
+        quaternion[3] = new Quaternion(0, 0, -1 ,0);
+        quaternion[4] = new Quaternion(-0.5f, -0.5f, -0.5f, 0.5f);
+        quaternion[5] = new Quaternion(0, 0 ,0, 1);
+        quaternion[6] = new Quaternion(0f, 0.707f, 0f, 0.707f);
 
         while (true){
             // get the list of active target id
             List<Integer> list = api.getActiveTargets();
 
             // move to a point
-            Point point = new Point(10.4d, -10.1d, 4.47d);
-            Quaternion quaternions = new Quaternion(0f, 0f, 0f, 1f);
-            api.moveTo(point, quaternions, false);
+//            Point point = new Point(10.4d, -10.1d, 4.47d);
+//            Quaternion quaternion = new Quaternion(0f, 0f, 0f, 1f);
+//            api.moveTo(point, quaternion, false);
+            MoveTo(point[0], quaternion[0]);
+            Log.i("target", "1 ");
+            MoveTo(point[1], quaternion[1]);
+            Log.i("target", "2 ");
+            MoveTo(point[2], quaternion[2]);
+            Log.i("target", "3 ");
+            MoveTo(point[3], quaternion[3] );
+            Log.i("target", "4 ");
+            MoveTo(point[4], quaternion[4] );
+            Log.i("target", "5 ");
+            MoveTo(point[5], quaternion[5] );
+            Log.i("target", "6 ");
+            MoveTo(point[6], quaternion[6]);
+            Log.i("target", "7 ");
 
             // get a camera image
-            Mat image = api.getMatNavCam();
-
+//            Mat image = api.getMatNavCam();
             // irradiate the laser
-            laserControl(true);
+
 
             // take active target snapshots
-            int target_id = 1;
-            api.takeTargetSnapshot(target_id);
+//            int target_id = 1;
+//            api.takeTargetSnapshot(target);
 
             /* ************************************************ */
             /* write your own code and repair the ammonia leak! */
@@ -86,16 +85,16 @@ public class YourService extends KiboRpcService {
                 break;
             }
 
-            loop_counter++;
-            if (loop_counter == 2){
-                break;
-            }
+//            loop_counter++;
+//            if (loop_counter == 2){
+//                break;
+//            }
         }
         // turn on the front flash light
         api.flashlightControlFront(0.05f);
-        
+
         // get QR code content
-//        String mQrContent = yourMethod();
+        String mQrContent = yourMethod();
 
         // turn off the front flash light
         api.flashlightControlFront(0.00f);
@@ -108,12 +107,12 @@ public class YourService extends KiboRpcService {
         /* ********************************************************** */
 
         // send mission completion
-//        api.reportMissionCompletion(mQrContent);
+        api.reportMissionCompletion(mQrContent);
     }
 
     @Override
     protected void runPlan2(){
-       // write your plan 2 here
+        // write your plan 2 here
     }
 
     @Override
@@ -121,16 +120,27 @@ public class YourService extends KiboRpcService {
         // write your plan 3 here
     }
 
-    private void laserControl(boolean on) {
-        api.laserControl(on);
-        return;
-    }
     // You can add your method
-    private void moveToPoint(double pos_x, double pos_y, double pos_z,
-                             double qua_x, double qua_y, double qua_z, double qua_w) {
-        final Point point = new Point(pos_x, pos_y, pos_z);
-        final Quaternion quaternion = new Quaternion((float)qua_x, (float)qua_y, (float)qua_z, (float)qua_w);
-        api.moveTo(point, quaternion, true);
-        return;
+    private String yourMethod(){
+        return "your method";
+    }
+
+    private void MoveTo(Point point, Quaternion quaternion){
+        Result result;
+        int count = 0, max_count = 4;
+
+        do
+        {
+            result = api.moveTo(point, quaternion, true);
+            count++;
+        }
+        while (!result.hasSucceeded() && count < max_count);
+        api.laserControl(true);
     }
 }
+
+
+
+
+
+
